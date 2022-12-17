274ce22eafe8d2d40f28783da0460cf8ad936477
Nahid51 <mdnahidyn@gmail.com>
2022-12-18 00:35:30 +0600

    added hero section in homepage

--- ezymart-client/pages/index.js
+++ ezymart-client/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import { ButtonGroup, Container, Dropdown, DropdownButton, Nav } from 'react-bootstrap';
+import { Carousel, Container, Dropdown, DropdownButton, Nav } from 'react-bootstrap';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Welcome from '../components/Welcome';
@@ -68,7 +68,139 @@ export default function Home() {
               </Nav>
             </div>
             <div className='col-12 col-lg-8'>
-              nahid
+              <div className='row gx-0'>
+                <div className='col-12 col-md-8'>
+                  <Carousel fade pause='hover'>
+
+                    <Carousel.Item interval={3000}>
+                      <img
+                        className="d-block w-100"
+                        src="https://i.ibb.co/23D2DD2/banner8-h18.jpg"
+                        alt="First slide"
+                      />
+                      <Carousel.Caption className='text-start'>
+                        <p>CURVED 4K UHD TV</p>
+                        <h3>Active Crystal Color 4k UHD Picture Quality</h3>
+                        <small>FROM $69.99</small>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+
+                    <Carousel.Item interval={3000}>
+                      <img
+                        className="d-block w-100"
+                        src="https://i.ibb.co/9v7sPnL/banner5-h18.jpg"
+                        alt="Second slide"
+                      />
+                      <Carousel.Caption className='text-start'>
+                        <p>ELECTRONICS</p>
+                        <h3>Best For Your Home</h3>
+                        <small> Starting at $59.99</small>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+
+                    <Carousel.Item interval={3000}>
+                      <img
+                        className="d-block w-100"
+                        src="https://i.ibb.co/SBQNsxD/banner6-h18.jpg"
+                        alt="Third slide"
+                      />
+
+                      <Carousel.Caption className='text-start'>
+                        <p>BEST SELLER</p>
+                        <h3>Luxury Watches</h3>
+                        <small>Starting at $59.99</small>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+
+                    <Carousel.Item interval={3000}>
+                      <img
+                        className="d-block w-100"
+                        src="https://i.ibb.co/w629ghT/banner7-h18.jpg"
+                        alt="Fourth slide"
+                      />
+                      <Carousel.Caption className='text-start'>
+                        <p>NEW STEP</p>
+                        <h3>Apple iPhone</h3>
+                        <small>Starting at $59.99</small>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+
+                  </Carousel>
+                  <div className="row gx-0">
+                    <div className='col-4'>
+                      <Carousel controls={false} indicators={false}>
+                        <Carousel.Item interval={3000}>
+                          <img
+                            className="d-block w-100"
+                            src="https://i.ibb.co/T8w6QgF/banner2-h18.jpg"
+                            alt="First photo"
+                          />
+                          <Carousel.Caption className='d-flex flex-column p-0'>
+                            <small className='fs-6'>Need Exclusive Perfumes</small>
+                            <small>
+                              <Link href="/" className='text-light'>Explore</Link>
+                            </small>
+                          </Carousel.Caption>
+                        </Carousel.Item>
+                      </Carousel>
+
+                    </div>
+                    <div className='col-4'>
+                      <Carousel controls={false} indicators={false}>
+                        <Carousel.Item interval={3000}>
+                          <img
+                            className="d-block w-100"
+                            src="https://i.ibb.co/t8d4BtV/banner3-h18.jpg"
+                            alt="Second photo"
+                          />
+                          <Carousel.Caption className='d-flex flex-column p-0'>
+                            <small className='fs-6'>Blender Combo</small>
+                            <small>
+                              <Link href="/" className='text-light'>Start Buying</Link>
+                            </small>
+                          </Carousel.Caption>
+                        </Carousel.Item>
+                      </Carousel>
+                    </div>
+                    <div className='col-4'>
+                      <Carousel controls={false} indicators={false}>
+                        <Carousel.Item interval={3000}>
+                          <img
+                            className="d-block w-100"
+                            src="https://i.ibb.co/dLdVfTY/banner4-h18.jpg"
+                            alt="Third photo"
+                          />
+                          <Carousel.Caption className='d-flex flex-column p-0'>
+                            <small className='fs-6'>Unio Leather Bags</small>
+                            <small>
+                              <Link href="/" className='text-light'>Shop now</Link>
+                            </small>
+                          </Carousel.Caption>
+                        </Carousel.Item>
+                      </Carousel>
+                    </div>
+                  </div>
+                </div>
+                <div className='col-12 col-md-4 displayPhoto'>
+                  <Carousel controls={false} indicators={false}>
+                    <Carousel.Item interval={3000}>
+                      <img
+                        className="d-block w-100"
+                        src="https://i.ibb.co/7JdHhDy/banner1-h18.jpg"
+                        alt="Fourth slide"
+                      />
+                      <Carousel.Caption className='text-start'>
+                        <small className='text-danger fw-bold'>NEW SHOTES</small>
+                        <h3 className='text-dark fw-bold mt-4 mb-5'>Best Price & Great Quality</h3>
+                        <small>
+                          <Link href="/" className='text-dark fw-bold fs-5'>View Product</Link>
+                        </small>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                  </Carousel>
+                </div>
+
+              </div>
             </div>
           </div>
         </Container>
