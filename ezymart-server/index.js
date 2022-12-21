af5b01d72cf06ea0119bb233808e16cdd3dd70f3
Nahid51 <mdnahidyn@gmail.com>
2022-12-21 22:01:53 +0600

    server site connected with mongodb database

--- ezymart-server/index.js
+++ ezymart-server/index.js
@@ -4,3 +4,20 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
+const port = process.env.PORT || 8080;
+
+// middleware
+app.use(express.json());
+
+// database connection
+main().then(() => console.log("Database connected!"));
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.pf4kpjf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`);
+}
+
+app.listen(port, () => {
+    console.log(`Server running at port ${port}`)
+})
+app.get("/", (req, res) => res.send('Document founded'));
